7704db44d5d2694bdcc9defc2f537854e265125f
John Anderson <jagh1729@gmail.com>
2018-12-05 14:36:24 +0100

    Create installBasicPrerequisites.sh

---
+++ jetsonTK1/buildTensorFlow/installBasicPrerequisites.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Basic Requirements:
+sudo apt-get install swig
+sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev
+
+## Protobuf Prerequisites
+sudo apt-get install git zip unzip autoconf automake libtool curl zlib1g-dev
