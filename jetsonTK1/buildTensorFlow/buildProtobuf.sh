842a4ba14544c47828250ff9e2cd906c1d281d12
John Anderson <jagh1729@gmail.com>
2018-12-05 14:42:32 +0100

    Create buildProtobuf.sh

---
+++ jetsonTK1/buildTensorFlow/buildProtobuf.sh
@@ -0,0 +1,19 @@
+## 2. Build Protobuf
+
+## Download the latest protobuf source from github:
+sudo chown -R ubuntu:ubuntu /opt
+cd /opt
+git clone https://github.com/google/protobuf.git
+
+## Checkout to get libprotoc in the version 3.4.0 
+cd /opt/protobuf
+git checkout tags/3.4.0
+
+## First generate the configuration file
+./autogen.sh
+./configure --prefix=/usr
+
+## Then run make
+make -j 4
+sudo make install
+protoc --version
