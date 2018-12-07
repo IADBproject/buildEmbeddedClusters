5bb29ded948402fa55bb676ca5d676482a6c7c02
John Anderson <jagh1729@gmail.com>
2018-12-07 11:50:32 +0100

    Update 0-installPrerequisites.sh

--- jetsonTK1/buildTensorFlow/0-installPrerequisites.sh
+++ jetsonTK1/buildTensorFlow/0-installPrerequisites.sh
@@ -3,6 +3,7 @@
 ## Basic Requirements:
 sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev
 sudo apt-get install swig libcurl3-dev python-numpy
+sudo pip install -U numpy
 
 ## Protobuf Prerequisites
 sudo apt-get install git zip unzip autoconf automake libtool curl zlib1g-dev
