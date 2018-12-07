2e3fcba5dee838e7419338e2b2241740b3c302e0
Phuc Le <37369211+phucle1303@users.noreply.github.com>
2018-12-07 12:10:14 +0100

    Update tf-0.8_quick_install.sh
    Put sudo on each command because we are using opt folder

--- jetsonTK1/pypa-tf/tf-0.8_quick_install.sh
+++ jetsonTK1/pypa-tf/tf-0.8_quick_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Install the basic requirements:
-chmod +x ../buildTensorFlow/*.sh
-./../buildTensorFlow/0-installPrerequisites.sh
+sudo chmod +x ../buildTensorFlow/*.sh
+sudo ./../buildTensorFlow/0-installPrerequisites.sh
 
 ## Install the CUDA-6.5, cuDNN and CUDA-7.0
-./../buildTensorFlow/5-installCUDA.sh
+sudo ./../buildTensorFlow/5-installCUDA.sh
 
 ## Faster installation for TensorFlow-0.8 on Jetson-TK1
 pip install tensorflow-0.8.0-cp27-none-linux_armv7l.whl
