bf73fb16b6ce8b5e43126fb4e62972e04586897a
ZiqingDu <dzqer@hotmail.com>
2019-06-19 17:09:33 +0200

    Updated command

--- jetsonTX2/tf-1.3_quick_install.sh
+++ jetsonTX2/tf-1.3_quick_install.sh
@@ -23,7 +23,7 @@ sudo pip3.6 install tensorflow-1.3.0-cp36-cp36m-linux_aarch64.whl
 
 #3) Install Keras
 sudo apt-get install libhdf5-serial-dev
-sudo pip3.6 install keras
+sudo pip3.6 install --upgrade keras==2.1.3
 
 #Alternative:
 #git clone https://github.com/keras-team/keras.git
@@ -34,3 +34,21 @@ sudo pip3.6 install mpi4py
 
 #5) install pandas
 sudo -H pip3.6 install pandas
+
+#6) Matplotlib
+sudo apt-get install libfreetype6-dev libpng-dev
+## sudo pip3.6 install matplotlib
+## or
+sudo apt-get install python3-matplotlib
+
+#7) ScikitLearn
+## sudo pip3.6 install --force-reinstall --no-cache-dir scikit-learn==0.21.1
+## or
+sudo pip3.6 install --upgrade setuptools
+sudo pip3.6 install -U setuptools
+sudo apt-get install libpcap-dev libpq-dev
+sudo pip3.6 install cython
+## and
+sudo pip3.6 install scikit-learn==0.21.1
+## or
+## sudo pip3 install git+https://github.com/scikit-learn/scikit-learn.git
