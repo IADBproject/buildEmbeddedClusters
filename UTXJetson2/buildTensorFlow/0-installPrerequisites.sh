cc5968b45f213eb9adc4e55658a63b716154d589
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2019-07-01 14:14:57 +0200

    install without prompt (apt-get-y)

--- UTXJetson2/buildTensorFlow/0-installPrerequisites.sh
+++ UTXJetson2/buildTensorFlow/0-installPrerequisites.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Bazel install prerequisites
-sudo apt install pkg-config zip g++ zlib1g-dev unzip python3
+sudo apt install -y pkg-config zip g++ zlib1g-dev unzip python3
 
 # TensorFlow build prerequisites
-sudo apt install python3-dev python3-pip
+sudo apt install -y python3-dev python3-pip
 python3 -m pip install -U --user pip six numpy wheel setuptools mock 'future>=0.17.1'
 python3 -m pip install -U --user keras_applications==1.0.6 --no-deps
 python3 -m pip install -U --user keras_preprocessing==1.0.5 --no-deps
