f915eb37ed9ab02bbca4b6ba2f3a847b5a3a4b66
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2019-07-19 23:13:21 +0200

    Fixes for the UTX installation

--- UTXJetson2/installWithoutCustomTF/global_scripts/installAllNodes.sh
+++ UTXJetson2/installWithoutCustomTF/global_scripts/installAllNodes.sh
@@ -18,29 +18,48 @@ NEW_USERNAME='mpiuser'
 NEW_PW='mpiuser'
 LOG_DIR="logs"
 
+function getHostname () {
+    hostname="$USERNAME@$1"
+    echo "$hostname"
+}
+
+function getLogFile () {
+    echo "$LOG_DIR/log_$1.txt"
+}
+
 function setUpUserOnNode () {
     ip_addr=$1
-    LOGFILE="$LOG_DIR/log_$ip_addr.txt"
-    hostname="$USERNAME@$ip_addr"
+    LOGFILE=$(getLogFile $ip_addr)
+    hostname=$(getHostname $ip_addr)
     echo "Creating new user on $hostname..." > "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" ssh -o StrictHostKeyChecking=accept-new "$hostname" "echo '$SUDO_PW' | sudo -HS useradd -m '$NEW_USERNAME'" >> "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" ssh "$hostname" "echo '$SUDO_PW' | sudo -HS adduser '$NEW_USERNAME' sudo" >> "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" ssh "$hostname" "echo '$SUDO_PW' | sudo -HS bash -c \"echo '$NEW_USERNAME:$NEW_PW' | chpasswd\"" >> "$LOGFILE" 2>&1
 }
 
-function installNode () {
+function sendScriptsToNode () {
     ip_addr=$1
-    LOGFILE="$LOG_DIR/log_$ip_addr.txt"
-    hostname="$USERNAME@$ip_addr"
+    LOGFILE=$(getLogFile $ip_addr)
+    hostname=$(getHostname $1)
     echo "Copying scripts to host $hostname..." >> "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" scp -o StrictHostKeyChecking=accept-new -r scripts/ "$IP_ADDRESSES_FILE" "$MASTER_IP_ADDRESSES_FILE" "$hostname": >> "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" ssh "$hostname" "chmod +x scripts/*.sh" scripts/ >> "$LOGFILE" 2>&1
+}
+
+function installNode () {
+    ip_addr=$1
+    LOGFILE=$(getLogFile $ip_addr)
+    hostname="$USERNAME@$ip_addr"
+
+    sendScriptsToNode $ip_addr
+
     echo "Installing on host $hostname..." >> "$LOGFILE" 2>&1
     sshpass -p "$SUDO_PW" ssh "$hostname" "echo '$SUDO_PW' | sudo -HS bash scripts/runAll.sh $2" >> "$LOGFILE" 2>&1
 }
 
 mkdir -p "$LOG_DIR"
 
+echo "Setting up user $NEW_USERNAME..."
 while read ip_addr; do
     setUpUserOnNode $ip_addr &
 done < "$IP_ADDRESSES_FILE"
@@ -51,13 +70,27 @@ USERNAME="$NEW_USERNAME"
 SUDO_PW="$NEW_PW"
 
 i=0
+
+# first install the NFS masters
+echo "Setting up the NFS masters..."
 while read ip_addr; do
-    if [[ $i -eq 7 || $i -eq 15 || $i -eq 23 ]]; then
-        isMaster=1
-    else
-        isMaster=0
+    installNode $ip_addr 1 &
+done < "$MASTER_IP_ADDRESSES_FILE"
+
+wait
+
+# then install the other nodes
+echo "Setting up the workers..."
+while read ip_addr; do
+    isMaster=0
+    while read master_addr; do
+        if [[ "$ip_addr" == "$master_addr" ]]; then
+            isMaster=1
+        fi
+    done < "$MASTER_IP_ADDRESSES_FILE"
+    if [[ "$isMaster" == 0 ]]; then
+        installNode $ip_addr 0 &
     fi
-    installNode $ip_addr $isMaster &
 done < "$IP_ADDRESSES_FILE"
 
 wait
--- UTXJetson2/installWithoutCustomTF/scripts/setUpNFSServer.sh
+++ UTXJetson2/installWithoutCustomTF/scripts/setUpNFSServer.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-sudo apt-get install nfs-kernel-server
-sudo mkdir /exports
+BLOCKDEV='/dev/sda'
+
+apt-get install nfs-kernel-server
+
+echo "Checking if formatted..."
+# prepare the file system
+if [[ $(file -s "$BLOCKDEV") != *"ext4"* ]]; then
+    echo "Formatting $BLOCKDEV..."
+    # the file system isn't formatted as needed, do it
+    mkfs.ext4 "$BLOCKDEV"
+fi
+
+# mount it
+mkdir -p /exports
 echo '/dev/sda /exports ext4 defaults 0 2' | tee -a /etc/fstab
-sudo mount /exports
+mount /exports
+
+# export it
 echo '/exports 134.59.132.0/24(rw,fsid=0,insecure,no_subtree_check,async)' | tee -a /etc/exports
 systemctl restart nfs-kernel-server.service
