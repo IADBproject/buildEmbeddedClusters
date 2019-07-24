492b0733bce563d0b0d1901989a01fbf61c42b49
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2019-07-24 11:15:05 +0200

    Add nodes to /etc/hosts

--- UTXJetson2/installWithoutCustomTF/scripts/miscConfig.sh
+++ UTXJetson2/installWithoutCustomTF/scripts/miscConfig.sh
@@ -8,4 +8,16 @@ mpiuser ALL = NOPASSWD: /usr/bin/killall
 mpiuser ALL = NOPASSWD: /usr/sbin/iftop' >> /etc/sudoers.d/diagnosenet
 
 # hostname e.g "astro03"
-hostnamectl set-hostname $(printf 'astro%02d' "$1")
+newname=$(printf 'astro%02d' "$1")
+hostnamectl set-hostname "$newname"
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "127.0.1.1 $newname" >> /etc/hosts
+
+# add all nodes to /etc/hosts
+IP_ADDRESSES_FILE="$(dirname $BASH_SOURCE)/../node_ip_addresses.txt"
+
+i=0
+while read ip_addr; do
+    echo "$ip_addr astro$i" >> /etc/hosts
+    i=$(( $i + 1 ))
+done < "$IP_ADDRESSES_FILE"
