0117f0d7cb0cd7b0943bb4370d4ddc936290b9ef
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2019-07-25 14:23:18 +0200

    Fixes for sudoers and NFS

--- UTXJetson2/installWithoutCustomTF/scripts/miscConfig.sh
+++ UTXJetson2/installWithoutCustomTF/scripts/miscConfig.sh
@@ -2,10 +2,10 @@
 
 # give mpiuser special permissions
 echo '## enerGyPU add Not pwd to recording the Jetson Stats
-mpiuser ALL = NOPASSWD: /home/mpiuser/cloud/diagnosenet/enerGyPU/dataCapture/enerGyPU_record-jetson.sh
-mpiuser ALL = NOPASSWD: /home/mpiuser/cloud/diagnosenet/enerGyPU/dataCapture/tegrastats
+mpiuser ALL = NOPASSWD: /home/mpiuser/cloud/0/diagnosenet/enerGyPU/dataCapture/enerGyPU_record-jetson.sh
+mpiuser ALL = NOPASSWD: /home/mpiuser/cloud/0/diagnosenet/enerGyPU/dataCapture/tegrastats
 mpiuser ALL = NOPASSWD: /usr/bin/killall
-mpiuser ALL = NOPASSWD: /usr/sbin/iftop' >> /etc/sudoers.d/diagnosenet
+mpiuser ALL = NOPASSWD: /usr/sbin/iftop' > /etc/sudoers.d/diagnosenet
 
 # hostname e.g "astro03"
 newname=$(printf 'astro%02d' "$1")
--- UTXJetson2/installWithoutCustomTF/scripts/setUpNFSClient.sh
+++ UTXJetson2/installWithoutCustomTF/scripts/setUpNFSClient.sh
@@ -4,11 +4,15 @@ MASTER_ADDRESSES_FILE="$(dirname $BASH_SOURCE)/../master_ip_addresses.txt"
 
 apt-get install -y nfs-common
 
+# reset the fstab
+echo > /etc/fstab
+
 i=0
 while read ip_addr; do
     mountpoint="/home/mpiuser/cloud/$i"
     mkdir -p "$mountpoint"
-    mount -t nfs -o proto=tcp,port=2049 "$ip_addr:/exports" "$mountpoint"
+    echo "$ip_addr:/exports $mountpoint nfs proto=tcp,port=2049,user 0 0" >> /etc/fstab
+    mount "$mountpoint"
     i=$(( i+1 ))
 done < "$MASTER_ADDRESSES_FILE"
 
